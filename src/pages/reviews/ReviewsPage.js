1e372f0bebe00204801652cf58f9b1023e0493a8
aimansae <saeedaimann@gmail.com>
2023-02-26 17:25:56 +0000

    Add infinite scroll

--- src/pages/reviews/ReviewsPage.js
+++ src/pages/reviews/ReviewsPage.js
@@ -12,6 +12,8 @@ import { axiosReq } from "../../api/axiosDefaults";
 import Review from "./Review";
 import NoResults from "../../assets/no-results.png";
 import Asset from "../../components/Asset";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 
 function ReviewsPage({ message, filter = "" }) {
   const [reviews, setReviews] = useState({ results: [] });
@@ -23,7 +25,9 @@ function ReviewsPage({ message, filter = "" }) {
   useEffect(() => {
     const fetchReviews = async () => {
       try {
-        const { data } = await axiosReq.get(`/reviews/?${filter}search=${search}`);
+        const { data } = await axiosReq.get(
+          `/reviews/?${filter}search=${search}`
+        );
         setReviews(data);
         setHasLoaded(true);
       } catch (err) {
@@ -32,13 +36,12 @@ function ReviewsPage({ message, filter = "" }) {
     };
     setHasLoaded(false);
     const timer = setTimeout(() => {
-        fetchReviews();
-    }, 1000)
+      fetchReviews();
+    }, 1000);
 
-    return () =>{
-        clearTimeout(timer)
-    }
-    
+    return () => {
+      clearTimeout(timer);
+    };
   }, [filter, search, pathname]);
 
   return (
@@ -46,26 +49,31 @@ function ReviewsPage({ message, filter = "" }) {
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular reviews mobile</p>
         <i className={`fas fa-search ${styles.SearchIcon}`} />
-          <Form
-            className={styles.SearchBar}
-            onSubmit={(event) => event.preventDefault()}
-          >
-            <Form.Control
-              value={search}
-              onChange={(event) => setSearch(event.target.value) }
-              type="text"
-              className="mr-sm-2"
-              placeholder="Search Reviews"
-            />
-          </Form>
-        
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search Reviews"
+          />
+        </Form>
 
         {hasLoaded ? (
           <>
             {reviews.results.length ? (
-              reviews.results.map((review) => (
-                <Review key={review.id} {...review} setReviews={setReviews} />
-              ))
+              <InfiniteScroll
+                children={reviews.results.map((review) => (
+                  <Review key={review.id} {...review} setReviews={setReviews} />
+                ))}
+                dataLength={reviews.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!reviews.next}
+                next={() => fetchMoreData(reviews,setReviews)}
+              />
             ) : (
               <Container className={appStyles.Content}>
                 <Asset src={NoResults} />
