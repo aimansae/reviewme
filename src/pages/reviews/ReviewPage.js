3c5ec43630aa3900de145663a7688474e178612a
aimansae <saeedaimann@gmail.com>
2023-02-28 20:06:03 +0000

    Add comment component

--- src/pages/reviews/ReviewPage.js
+++ src/pages/reviews/ReviewPage.js
@@ -8,19 +8,27 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Review from "./Review";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../context/CurrentUserContext";
+import Comment from "../comments/Comment";
 
 function ReviewPage() {
   const { id } = useParams();
   const [review, setReview] = useState({ results: [] });
 
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
+
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: review }] = await Promise.all([
+        const [{ data: review }, {data:comments}] = await Promise.all([
           axiosReq.get(`/reviews/${id}`),
+          axiosReq.get(`/comments/?review=${id}`),
         ]);
         setReview({ results: [review] });
-        console.log(review);
+        setComments(comments)
       } catch (err) {
         console.log(err);
       }
@@ -34,7 +42,29 @@ function ReviewPage() {
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
         <Review {...review.results[0]} setReviews={setReview} reviewPage />
-        <Container className={appStyles.Content}>Comments</Container>
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setReview={setReview}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+          {comments.results.length ? (
+            comments.results.map(comment => {
+              <Comment key={comment.id} {...comment} />
+            })
+          ) : currentUser? (
+            <span>No comments.. Leave a comment</span>
+          ):(
+            <span>No comments yet... Please Log In to view more</span>
+          )
+           }
+        </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
